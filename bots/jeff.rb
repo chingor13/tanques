7d5302d160cc732e3f2d25d6743d648aa8c717f4
Jeff Ching <jching@avvo.com>
2013-05-10 23:51:48 -0700

    can beat seek and destroy

--- bots/jeff.rb
+++ bots/jeff.rb
@@ -4,15 +4,67 @@ class Jeff < RTanque::Bot::Brain
 
   def tick!
     ## main logic goes here
-    
-    # use self.sensors to detect things
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Sensors
-    
-    # use self.command to control tank
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Command
-    self.command.speed = 3
-    
-    # self.arena contains the dimensions of the arena
-    # See http://rubydoc.info/github/awilliams/RTanque/master/frames/RTanque/Arena
+    pick_spot!
+    turn_to_spot!
+    acquire_target!
+    fire_when_ready!    
   end
+
+  protected
+
+  def at_spot?
+    return false if @spot.nil?
+
+    self.sensors.position.within_radius?(@spot, 10)
+  end
+
+  def pick_spot!
+    @spot = nil if at_spot?
+    if @spot.nil?
+      @spot = RTanque::Point.new(rand(self.arena.width), rand(self.arena.height), self.arena)
+    end
+  end
+
+  def turn_to_spot!
+    self.command.heading = self.sensors.position.heading(@spot)
+    if self.sensors.position.distance(@spot) < 15
+      self.command.speed = 1
+    else
+      self.command.speed = 3
+    end
+  end
+
+  def acquire_target!
+    if @target = self.sensors.radar.sort_by(&:distance).first
+      # lock radar and turret on target
+      self.command.radar_heading = @target.heading
+      self.command.turret_heading = @target.heading
+    else
+      # naively spit left
+      self.command.radar_heading = self.sensors.radar_heading - RTanque::Heading::EIGHTH_ANGLE
+    end
+  end
+
+  def fire_when_ready!
+    if @target
+      if (self.sensors.turret_heading.to_degrees - @target.heading.to_degrees).abs < 2
+        # control your firepower
+        if @target.distance < 250
+          self.command.fire(5)
+        elsif @target.distance < 500
+          self.command.fire(10)
+        else
+          self.command.fire(1)
+        end
+      end
+    end
+  end
+
+  def calculate_target_position(target)
+    RTanque::Point.new(
+      self.sensors.position.x + Math.cos(target.heading) * target.distance,
+      self.sensors.position.y + Math.sin(target.heading) * target.distance
+    )
+  end
+
 end
