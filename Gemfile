aca28d945b3e731ec92dd698cb740af87e9f315f
Jeff Ching <jching@avvo.com>
2013-05-10 22:34:16 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'rtanque', :github => 'awilliams/RTanque'
---
+++ bots/jeff.rb
@@ -0,0 +1,18 @@
+class Jeff < RTanque::Bot::Brain
+  NAME = 'jeff'
+  include RTanque::Bot::BrainHelper
+
+  def tick!
+    ## main logic goes here
+    
+    # use self.sensors to detect things
+    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Sensors
+    
+    # use self.command to control tank
+    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Command
+    self.command.speed = 3
+    
+    # self.arena contains the dimensions of the arena
+    # See http://rubydoc.info/github/awilliams/RTanque/master/frames/RTanque/Arena
+  end
+end
